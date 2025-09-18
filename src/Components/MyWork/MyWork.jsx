488329ec49844ea2ff881873deb3dfbe9bb2e4f5
sollyaj <soajohworode@gmail.com>
2025-09-18 16:43:18 +0100

    update

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -6,20 +6,32 @@ import arrow_icon from '../../assets/arrow_icon.svg'
 
 const Mywork = () => {
   return (
-        <div id='work' className="mywork">
-            <div className='mywork-title'>
-                <h1>My latest work</h1>
-                <img src={theme_pattern} alt="" />
-            </div>
-            <div className='mywork-container'>
-                {mywork_data.map((work,index)=>{
-                    return <img key={index} src={work.w_img} alt="" />
-                })}
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt="" />
-        </div>
+    <div id='work' className="mywork">
+      <div className='mywork-title'>
+        <h1>My latest work</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+
+      <div className='mywork-container'>
+        {mywork_data.map((work, index) => (
+          <div className="work-item" key={index}>
+            {work.link ? (
+              <a href={work.link} target="_blank" rel="noopener noreferrer">
+                <img src={work.w_img} alt={work.w_name} />
+              </a>
+            ) : (
+              <img src={work.w_img} alt={work.w_name} />
+            )}
+            <h3>{work.w_name}</h3>
+            {work.description && <p>{work.description}</p>}
+          </div>
+        ))}
+      </div>
+
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
     </div>
   )
 }
